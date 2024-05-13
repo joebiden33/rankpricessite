bd0ba1d1018f8c8fb65808863063744e3f7bc510
joebiden33 <163921903+joebiden33@users.noreply.github.com>
2024-05-13 22:01:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function showsvdzialki() {
+    document.getElementById('#svdzialki').style.display = 'table';
+    document.getElementById('#duels').style.display = 'none';
+    document.getElementById('#skyblock').style.display = 'none';
+  }
+  
+  function showduels() {
+    document.getElementById('#svdzialki').style.display = 'none';
+    document.getElementById('#duels').style.display = 'table';
+    document.getElementById('#skyblock').style.display = 'none';
+  }
+  
+  function showskyblock() {
+    document.getElementById('#svdzialki').style.display = 'none';
+    document.getElementById('#duels').style.display = 'none';
+    document.getElementById('#skyblock').style.display = 'table';
+  }
+  
